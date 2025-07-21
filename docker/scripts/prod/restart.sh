25d985a4f04e2136e70ff80e4861642e51860332
Piotr Czech <czechpiotr2701@gmail.com>
2025-07-21 20:14:14 +0200

    feat(core): add docker prod scripts

---
+++ docker/scripts/prod/restart.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose.prod.yml build --no-cache app queue-worker schedule-worker nginx && docker compose -f docker-compose.prod.yml up -d --force-recreate --no-deps app queue-worker schedule-worker nginx
